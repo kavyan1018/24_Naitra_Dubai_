ebb2f30e348496c93ba9eb5a8309dc94e962802b
Kavyan Nayak <kavyannayak104@gmail.com>
2024-04-30 20:55:32 +0530

    New

---
+++ C/IF_ELSE.C
@@ -0,0 +1,47 @@
+// #include<stdio.h>
+// #include<conio.h>
+
+// int main()
+// {
+//     // find maximum number between 2 number
+
+//     int a,b;
+
+//     printf("Enter The Frist Numner :-");
+//     scanf("%d",&a);
+
+//     printf("Enter The Second Numner :-");
+//     scanf("%d",&b);
+
+//     if (a > b)
+//     {
+//         printf("A is Max");
+//     }
+//     else
+//     {
+//         printf("B is Max");
+//     }
+// }
+
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter the Number between 1 to 7 :-");
+    scanf("%d",&a);
+
+    if (a == 1)
+    {
+        printf("The day is Monday");
+    }
+    if (a == 2)
+    {
+        printf("The day is Tuesday");
+    }
+    
+}
