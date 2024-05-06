69cfe01bf05e532c3f5c4fb9432541147643c0bf
Kavyan Nayak <kavyannayak104@gmail.com>
2024-05-06 21:12:21 +0530

    New

--- C/IF_ELSE.C
+++ C/IF_ELSE.C
@@ -48,7 +48,7 @@
 
 
 // else if 
-
+/*
 #include<stdio.h>
 #include<conio.h>
 
@@ -74,5 +74,44 @@ int main()
     {
         printf("Both are same !");
     }
+    
+}
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int units;
+    float amt,total_amt,sur_charge;
+
+    printf("Enter the Units :-");
+    scanf("%d",&units);
+
+
+    if (units <= 50)
+    {
+        amt = units * 0.50;
+    }
+    else if (units <= 100)
+    {
+        amt = 25 + ((units - 50) * 0.75);
+    }
+    else if (units <= 200)
+    {
+        amt = 75 + ((units - 100) * 1.20);
+    }
+    else if (units > 250)
+    {
+        amt = 120 + ((units - 200) * 1.50);
+    }
+
+    sur_charge = amt * 0.20;
+
+    total_amt = amt + sur_charge;
+
+    printf("Electricity Bill = Rs. %.2f",total_amt);
     
 }
